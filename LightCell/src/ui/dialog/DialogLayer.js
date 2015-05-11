2c64d5c1be7e5ebd13cee689f9ed24e7699bb677
刘健 <liujian890508@gmail.com>
2015-05-12 01:11:44 +0800

    add Dialog

---
+++ LightCell/src/ui/dialog/DialogLayer.js
@@ -0,0 +1,14 @@
+/**
+ * Created by liujian on 15-5-12.
+ */
+
+var ui_dialogLayer = ui_base.extend({
+
+    showDialog: function(){
+
+    },
+
+    hideDialog: function(){
+
+    }
+});
---
+++ LightCell/src/ui/dialog/DialogManager.js
@@ -0,0 +1,16 @@
+/**
+ * Created by liujian on 15-5-12.
+ */
+
+var DialogManager = function () {
+    var _dialog = {};
+
+    _dialog.showDialog = function(data, resultCallback){
+        var dialog = new ui_dialogLayer();
+        /**
+         * -------------------content
+         */
+        dialog.showDialog();
+        return dialog;
+    }
+}
